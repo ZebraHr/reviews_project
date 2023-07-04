64f76ddeb59ffad8f89a774dd3b6487eedffcfbe
viktorova olga <viktotovaol4ik@yandex.ru>
2023-07-04 10:47:53 +0300

    создание моделей и админки комментарии и отзывы

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Review, Comment
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'text', 'author', 'score', 'pub_date',)
+    empty_value_display = '-пусто-'
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'review', 'text', 'author', 'pub_date',)
+    list_filter = ('pub_date',)
+    search_fields = ('text',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
