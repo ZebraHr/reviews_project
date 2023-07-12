0a8271cffe970ae785a57fcdcf35d0130b759b82
vikolga <124797192+vikolga@users.noreply.github.com>
2023-07-12 22:28:10 +0300

    Update serializers.py

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,7 +2,6 @@ import uuid
 import datetime as dt
 
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import get_object_or_404
 from rest_framework.validators import UniqueValidator
 from rest_framework import serializers
 
@@ -96,11 +95,9 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         request = self.context['request']
-        title = get_object_or_404(
-            Title,
-            id=self.context['view'].kwargs.get('title_id'))
+        id = self.context['view'].kwargs.get('title_id')
         if request.method == 'POST':
-            if title.reviews.filter(author=request.user):
+            if request.user.reviews.filter(title__id=id):
                 raise ValidationError(
                     ErrorResponse.ONE_REVIEW_ONLY
                 )
